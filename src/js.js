c1449884cf3d97a80065ee36f3fe38bc4179c39d
wqMV <35995548@qq.com>
2019-01-04 14:41:04 +0800

    Signed-off-by: wqMV

--- src/js.js
+++ src/js.js
@@ -1,2 +1,3 @@
-var a = q("oD");
-console.log(a)
+var a = q(":oN").css({ color: "#f00", background: "#00f" });
+b = a[0].g("n", "tt");
+console.log(b);
--- src/wqMV.js
+++ src/wqMV.js
@@ -7,14 +7,15 @@
 "use strict";
 
 // q
-((W) => {
+(() => {
 	// S String, E Element
 	const q = (S, E = document.body) => {
+		let l = 0;
 		const s = (E) => {
-			if (!E.a) {
-				E.a = (S = "", V = "") => {
+			if (!E.g) {
+				E.g = (S = "", V = "") => {
 					if (S && V) E.setAttribute(S, V);
-					else return E.getAttribute(S) || "";
+					return E.getAttribute(S) || "";
 				};
 				E.c = E.textContent || "";
 				Object.defineProperties(E, {
@@ -24,28 +25,20 @@
 					},
 					v: {
 						get: () => { return E.value || "" },
-						set: (S = "") => { if (E.value) E.value = S }
+						set: (S = "") => { if (E.value !== "undefined") E.value = S }
 					}
 				});
 			}
 		};
 
 		if (S.startsWith(".")) E = E.getElementsByClassName(S.substr(1));
-		else if (S.startsWith("$")) E = document.getElementsByName(S.substr(1));
+		else if (S.startsWith(":")) E = document.getElementsByName(S.substr(1));
 		else if (S.startsWith("@")) E = E.getElementsByTagName(S.substr(1));
 		else if (S) E = document.getElementById(S);
 		if (!E || (E && E.length === 0)) return;
-		q.e = E;
-		//q.l = E.tagName === "SELECT" ? 0 : E.length;
-		//if (q.l) for (const i of E) s(i);
-		//else s(E);
-
-		/*
-		E.del = () => {
-			if (q.l) for (let i = 0; i < q.l; i++) E[0].parentElement.removeChild(E[0]);
-			else E.parentElement.removeChild(E);
-			return "";
-		};
+		l = E.tagName === "SELECT" ? 0 : E.length;
+		if (l) for (const i of E) s(i);
+		else s(E);
 
 		// S String, A Any
 		E.add = (S = "", A = "") => {
@@ -66,12 +59,34 @@
 			return E;
 		};
 
-		// N keycode,F Function
-		E.key = (F, N = 13) => {
+
+		// J Json or String
+		E.css = (J = "") => {
+			if (typeof J === "object") {
+				if (l) for (const i of E) for (const j in J) i.style[j] = J[j];
+				else for (const j in J) E.style[j] = J[j];
+			} else E.className = J;
+			return E;
+		};
+
+		E.del = () => {
+			if (l) for (let i = 0; i < l; i++) E[0].parentElement.removeChild(E[0]);
+			else E.parentElement.removeChild(E);
+			return "";
+		};
+
+		// F Function
+		E.for = (C) => {
+			if (l) for (const i of E) C(i);
+			return E;
+		};
+
+		// N keycode, C Callback
+		E.key = (C, N = 13) => {
 			const k = () => {
 				if (event.keyCode == N) {
 					event.returnValue = false;
-					if (typeof F === "function") F(event.target);
+					if (typeof C === "function") C(event.target);
 				}
 			};
 			if (l) for (const i of E) i.onkeydown = k;
@@ -79,38 +94,8 @@
 			return E;
 		};
 
-		*/
 		return E;
 	};
-
-	q.prototype = {
-		_name: "chen",
-		age: 21,
-		set h(name) { this._name = name; },
-		get h() { return this._name; }
-	};
-	console.log(q.prototype);
-	/*
-	Object.defineProperties(q.prototype, {
-		h: {
-			get: () => { alert(); return q.e.innerHTML || "" },
-			set: (S = "") => { q.e.innerHTML = S }
-		},
-		v: {
-			get: () => { return q.e.value || "" },
-			set: (S = "") => { if (q.e.value) q.e.value = S }
-		}
-	});
-	*/
-
-	var person = {
-		_name: "chen",
-		age: 21,
-		set name(name) { this._name = name; },
-		get name() { return this._name; }
-	}
-	console.log(person.name);
-
 
 	// N Name, V Value 0 delete
 	q.ss = (N, V) => {
@@ -120,6 +105,7 @@
 		return s.getItem(N);
 	};
 
+
 	Array.prototype.p = Array.prototype.push;
-	W.q = q;
-})(window);
+	window.q = q;
+})();
