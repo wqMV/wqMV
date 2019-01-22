06201dec049f4ed9476b7c2c82af06297457c4e7
wqMV <35995548@qq.com>
2019-01-22 18:26:38 +0800

    Signed-off-by: wqMV

--- src/wqEDR.js
+++ src/wqEDR.js
@@ -35,13 +35,15 @@
 		let h = [];
 
 		h.p(`<style type="text/css">\n`);
-		h.p(`#_wqEt select { margin: 0 5px; padding: 2px; border-radius: 3px }\n`);
 		h.p(`#_wqEt { padding: 5px 10px; border-top: 1px solid #999; border-left: 1px solid #999; border-right: 1px solid #999; border-radius: 5px 5px 0 0; box-shadow: 2px 2px 2px #666 }\n`);
-		h.p(`#_wqEr { padding: 10px; height: 24rem; overflow-y: scroll; outline: none; border: 1px solid #999; box-shadow: 2px 2px 2px #666 }\n`);
-		h.p(`.ed_ful { position: absolute; top: 0; left: 0; background: snow }\n`);
+		h.p(`#_wqEr { padding: 10px; height: 12rem; overflow-y: scroll; outline: none; border: 1px solid #999; box-shadow: 2px 2px 2px #666 }\n`);
+		h.p(`#_wqEt select { margin: 0 5px; padding: 2px; border-radius: 3px }\n`);
+		h.p(`#_wqEt > div { display:inline-block }\n`);
+		h.p(`.ed_ful { position: absolute; top: 0; left: 0; background: #000 }\n`);
+		h.p(`.ed_ful > div { background: snow }\n`);
 		h.p(`.ed_btn { display:inline-block; margin: 0 1px; padding: 1px; vertical-align: middle }\n`);
-		h.p(`.sept { width: 2px; height: 20px; background: url(../img/is.png) -400px 0 }\n`);
-		h.p(`.icon { width: 20px; height: 20px; border: 1px solid transparent; background-repeat: no-repeat; background:url(../img/is.png) }\n`);
+		h.p(`.sept { width: 2px; height: 20px; background: url("img/ed.png") -400px 0 }\n`);
+		h.p(`.icon { width: 20px; height: 20px; border: 1px solid transparent; background-repeat: no-repeat; background:url("img/ed.png") }\n`);
 		h.p(`.icon:hover, .icon:active { border: 1px solid #28d; background-color: #cff }\n`);
 		for (var i = 0; i < be.length; i++) h.p(`.${be[i]} { background-position: -${i * 20}px 0 }\n`);
 		h.p(`</style>\n`);
@@ -49,7 +51,6 @@
 		for (var i = 0; i < 3; i++) h.p(p(i));
 		h.p(s());
 		h.p(c("forecolor", "前景", cl));
-		h.p(s());
 		h.p(c("backcolor", "背景", cl));
 		h.p(s());
 		h.p(c("formatblock", "段落", {
@@ -59,7 +60,6 @@
 			h2: "标题2",
 			h3: "标题3"
 		}));
-		h.p(s());
 		h.p(c("fontsize", "大小", {
 			"1": "很小",
 			"2": "小",
@@ -69,7 +69,7 @@
 			"6": "特大",
 			"7": "最大"
 		}));
-		h.p(`</div> <div onmousedown="return false">`);
+		h.p(`</div><div onmousedown="return false">`);
 		for (var i = 3; i < 9; i++) h.p(p(i));
 		h.p(s());
 		for (var i = 9; i < 11; i++) h.p(p(i));
@@ -80,7 +80,7 @@
 		h.p(`</div></div>`);
 		h.p(`<div id="_wqEr" contenteditable="true" tabindex="0"></div>`);
 		O.style["text-align"] = "left";
-		O.style["padding"] = "10px";
+		O.style["padding"] = "5px";
 		O.innerHTML = h.join("");
 		ed.o = O;
 		ed.g = document.getElementById("_wqEr");
@@ -104,7 +104,7 @@
 
 	ed.sour = () => {
 		let o = document.getElementById("_wqEx"),
-			x = ed.g.clientWidth - 50, y = ed.g.clientHeight - 40, i = ed.g.innerHTML;
+			x = ed.g.clientWidth - 80, y = ed.g.clientHeight - 60, i = ed.g.innerHTML;
 		if (o) {
 			i = o.value;
 			ed.g.innerHTML = i;
@@ -119,11 +119,11 @@
 		if (ed.o.className) {
 			ed.o.className = "";
 			ed.o.style.width = "";
-			ed.g.style.height = "24rem";
+			ed.g.style.height = "12rem";
 		} else {
 			ed.o.className = "ed_ful";
-			ed.o.style.width = "calc(100vw - 20px)";
-			ed.g.style.height = "calc(100vh - 100px)";
+			ed.o.style.width = "calc(100vw - 10px)";
+			ed.g.style.height = "calc(100vh - 72px)";
 		}
 	};
 
